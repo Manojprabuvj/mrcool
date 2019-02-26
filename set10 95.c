2163b5483a27c01e6738a89e17e0b02fa8ea5758
Manojvijay <47790550+Manojprabuvj@users.noreply.github.com>
2019-02-26 09:12:29 -0800

    Create set10 95.c

---
+++ set10 95.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<math.h>
+int main(void) 
+{
+	float p,r,mj;
+	int s,n;
+	scanf("%f %d %f",&p,&n,&r);
+	mj=((p*n*r)/100);
+	s=floor(mj);
+	printf("\n%d",s);
+	return 0;
+}
