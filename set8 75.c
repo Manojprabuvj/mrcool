7ad94d47c399f483c1b91cdb5b949f336a9b449f
Manojvijay <47790550+Manojprabuvj@users.noreply.github.com>
2019-02-26 05:49:23 -0800

    Create set8 75.c

---
+++ set8 75.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) 
+{
+
+	char a[30];
+	int k,i;
+	scanf("%s",a);
+	k=strlen(a);
+	if(n%2==0)
+	{
+  
+		a[k/2]='*';
+		a[(k/2)-1]='*';
+	}
+	else
+	{
+		a[k/2]='*';
+	}
+	printf("\n%s",a);
+	return 0;
+}
