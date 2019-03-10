5ce65ad1ebc1558474d0de15a58f4e0ebdd24a9f
Manojvijay <47790550+Manojprabuvj@users.noreply.github.com>
2019-03-10 03:18:17 -0700

    Create p13.c

---
+++ p13.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int a,l,s=0,m;
+    scanf("%d",&a);
+    while(a)
+    {
+        l=a%10;
+        a=a/10;
+        
+    
+l=k*k;
+s=s+m;
+}
+printf("%d",s);
+}
